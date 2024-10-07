e588909beae3ab1edd7d3bbae996457f618e445d
farhan <ff1917914@gmail.com>
2024-10-07 04:44:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+
+
+
+// Q1
+
+// var date = new Date();
+
+// console.log(date);
+
+// Q2
+
+// function alertCurrentMonth() {
+//     var months = [
+//         "January", "February", "March", "April",
+//         "May", "June", "July", "August",
+//         "September", "October", "November", "December"
+//     ];
+//     var  currentMonthIndex = new Date().getMonth();
+//     alert(months[currentMonthIndex]);
+// }
+// alertCurrentMonth();
+
+
+// Q3
+
+// function alertCurrentDay() {
+   
+//     var  days = [
+//         "Sunday", "Monday", "Tuesday", "Wednesday",
+//         "Thursday", "Friday", "Saturday"
+//     ];
+
+   
+//     var  currentDayIndex = new Date().getDay();
+
+
+//     var  currentDayAbbr = days[currentDayIndex].substring(0, 3);
+
+    
+//     alert(currentDayAbbr);
+// }
+
+
+// alertCurrentDay();
